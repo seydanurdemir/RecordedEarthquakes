58961eb180731b21a818d4d61be7539d26b3cf0e
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2022-02-14 00:49:28 +0300

    Update Main.java, Add gson jar

--- src/com/seydanurdemir/Main.java
+++ src/com/seydanurdemir/Main.java
@@ -1,6 +1,15 @@
 package com.seydanurdemir;
 
+import com.seydanurdemir.model.Feature;
+import com.seydanurdemir.model.Root;
+import com.google.gson.Gson;
+
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Period;
@@ -37,5 +46,33 @@ public class Main {
         // Calculate the difference between start and end days
         Period diff = Period.between(t1, t2);
 
+        // Create new http client
+        HttpClient client = HttpClient.newHttpClient();
+
+        // Get request from the given api
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(String.format("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=%s&endtime=%s", startTimeStr, endTimeStr)))
+                .header("Content-Type", "application/json")
+                .build();
+
+        // Get response
+        HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
+
+        // Get json file and parse using Gson
+        Root obj = new Gson().fromJson(response.body(), Root.class);
+
+        if ((long) obj.features.size() == 0) {
+            // If there were no earthquakes happened in given {x} days.
+            System.out.printf("No Earthquakes were recorded past %d days.%n", diff.getDays());
+        } else {
+            // Otherwise, list country, place, magnitude, date and time
+            // of the earthquakes that happened in given {x} days.
+            System.out.printf("%n%d Earthquakes were recorded past %d days.%n %n", (long) obj.features.size(), diff.getDays());
+            for (Feature feature: obj.features) {
+                Timestamp timestamp = new Timestamp(feature.properties.time);
+                System.out.printf("%s, %.2f, %s %n", feature.properties.place, feature.properties.mag, simpleDateFormat.format(timestamp.getTime()));
+            }
+        }
     }
 }
